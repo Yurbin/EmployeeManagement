c0e20d4fd60be9c3abe027d8eabe7b1a08ed51af
许勇滨 <83461452@qq.com>
2018-02-11 14:07:08 +0800

    Initiall commit
    Signed-off-by: 许勇滨 <83461452@qq.com>

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyD63JJjIfAPpqhPGE_xqZFf5S33ozCPb_Y",
+  authDomain: "vuefs-prod-ff091.firebaseapp.com",
+  databaseURL: "https://vuefs-prod-ff091.firebaseio.com",
+  projectId: "vuefs-prod-ff091",
+  storageBucket: "vuefs-prod-ff091.appspot.com",
+  messagingSenderId: "835426502063"
+}
